d4b44aaebff3cbe9970e23398c7f960a599c801c
Elijah Nyasiando <143831019+mgkEdits@users.noreply.github.com>
2023-10-09 15:47:17 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -18,16 +18,20 @@ const init = () => {
 
                 // Add an event listener to each movie card
                 filmCard.addEventListener('click', () => {
-               
                     const allFilmCards = document.querySelectorAll('.card-wrap');
                     allFilmCards.forEach((card) => {
                     card.style.display = 'none';  // Hides all movie card
-                
                 });
-
                 // Displays the clicked movie card
                 filmCard.style.display = 'block';
-             });
+            });
+
+            // Add an event listener to the "Buy Ticket" button
+            const readMoreBtn = filmCard.querySelector('.btn');
+            readMoreBtn.addEventListener('click', (event) => {
+               event.preventDefault();
+               buyTicket(film, readMoreBtn);
+            });
 
             });
         })
@@ -36,98 +40,88 @@ const init = () => {
     // Function to create a card for a film
     function createFilmCard(film){
 
-    //elements to be created and added to the DOM
-    // div element class "card-wrap"
-    const cardWrap = document.createElement("div");
-    cardWrap.className = "card-wrap";
+      // div element class "card-wrap"
+      const cardWrap = document.createElement("div");
+      cardWrap.className = "card-wrap";
 
-    // div element class "card"
-    const card = document.createElement("div");
-    card.className = "card";
+      // div element class "card"
+      const card = document.createElement("div");
+      card.className = "card";
 
-    // div element class "card-content z-index"
-    const cardContent = document.createElement("div");
-    cardContent.className = "card-content z-index";
+      // div element class "card-content z-index"
+      const cardContent = document.createElement("div");
+      cardContent.className = "card-content z-index";
 
-    // img element for the icon
-    const iconImg = document.createElement("img");
-    iconImg.className = "icon";
-    iconImg.src = film.poster;
-    iconImg.alt = "";
+      // img element for the icon
+      const iconImg = document.createElement("img");
+      iconImg.className = "icon";
+      iconImg.src = film.poster;
+      iconImg.alt = "";
 
-    // h3 element for the title
-    const title = document.createElement("h3");
-    title.className = "title-sm";
-    title.textContent = film.title;
+      // h3 element for the title
+      const title = document.createElement("h3");
+      title.className = "title-sm";
+      title.textContent = film.title;
 
-    // paragraph element for the text
-    const text = document.createElement("p");
-    text.className = "text";
-    text.textContent = `Show Time: ${film.showtime} ||  Run Time: ${film.runtime}minutes `;
+      // paragraph element for the text
+      const text = document.createElement("p");
+      text.className = "text";
+      text.textContent = `Show Time: ${film.showtime} ||  Run Time: ${film.runtime}minutes `;
 
-    //  anchor element for the "Read more" button
-    const readMoreBtn = document.createElement("a");
-    readMoreBtn.href = "";
-    readMoreBtn.className = "btn small";
+      //  anchor element for the "Read more" button
+      const readMoreBtn = document.createElement("a");
+      readMoreBtn.href = "";
+      readMoreBtn.className = "btn small";
 
-    let totalTicket = film.capacity;
-    let ticketSold = film.tickets_sold;
-    const availTicket = totalTicket - ticketSold;
-    if (availTicket>0) {
-        readMoreBtn.textContent = `${availTicket} available tickets`;
-    } else {
-        readMoreBtn.textContent = `SOLD OUT!`;
-        readMoreBtn.style.pointerEvents = "none"; // Disable button if sold out
+      updateTicketButton(film, readMoreBtn);
+    
+      // Append the elements to their respective parent elements
+      cardContent.appendChild(iconImg);
+      cardContent.appendChild(title);
+      cardContent.appendChild(text);
+      cardContent.appendChild(readMoreBtn);
+
+      card.appendChild(cardContent);
+      cardWrap.appendChild(card);
+    
+      return cardWrap;
     }
 
-    // Add a click event listener to the "Buy Ticket" button
-    readMoreBtn.addEventListener("click", (event) => {
-        event.preventDefault();
-  
-        // Check if tickets are available
-        if (availTicket > 0) {
-          // Update the available tickets on the frontend
-          totalTicket++;
+    // Function to update the "Buy Ticket" button based on available tickets
+    function updateTicketButton(film, button) {
+        const availTicket = film.capacity - film.tickets_sold;
+          if (availTicket > 0) {
+            button.textContent = `${availTicket} available tickets`;
+            button.style.pointerEvents = 'auto'; // Enable the button
+          } else {
+            button.textContent = 'SOLD OUT!';
+            button.style.pointerEvents = 'none'; // Disable the button if sold out
+        }
+    }
 
+    // Function to handle the "Buy Ticket" button click
+    function buyTicket(film, button) {
+       const availTicket = film.capacity - film.tickets_sold;
+       if (availTicket > 0) {
+          film.tickets_sold++;
+          updateTicketButton(film, button);
 
           // Make a request to persist the updated number of tickets_sold
           fetch(`http://localhost:3000/films/${film.id}`, {
-            method: "PATCH",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ tickets_sold: totalTicket }),
-          })
-          .then((res) => res.json())
-          .then((data) => {
-            // Handle the response if needed
-            data.forEach(filmx => {
-                // Update the button text
-                if (availTicket > 0) {
-                    readMoreBtn.textContent = `${filmx.capacity-filmx.tickets_sold} available tickets`;
-                  } else {
-                    readMoreBtn.textContent = `SOLD OUT!`;
-                    readMoreBtn.style.pointerEvents = "none"; // Disable button if sold out
-            }
+              method: 'PATCH',
+              headers: {
+                  'Content-Type': 'application/json',
+                },
+              body: JSON.stringify({ tickets_sold: film.tickets_sold }),
             })
-          })
-
-            
-        }
-      });
-    
-
-    // Append the elements to their respective parent elements
-    cardContent.appendChild(iconImg);
-    cardContent.appendChild(title);
-    cardContent.appendChild(text);
-    cardContent.appendChild(readMoreBtn);
-
-    card.appendChild(cardContent);
-    cardWrap.appendChild(card);
-    
-    return cardWrap;
+        
+        .catch((error) => {
+          console.error('Error updating tickets_sold:', error);
+        });
     }
+  }
+
+
 
     getFilms();
 
